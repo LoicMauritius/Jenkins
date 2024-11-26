921dc1df2f5a41aeb074762d74445486ac5514ab
Mauritius Loic <123571057+LoicMauritius@users.noreply.github.com>
2024-11-26 08:31:05 +0100

    Update App.java
    Test for the automatic pipeline trigger

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        var msg = "Hello World!"
+        System.out.println(msg);
     }
 }
